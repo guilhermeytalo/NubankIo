caa7dc7ce4d9e5216e0f7db2376727dc7aa96eb6
Guilherme <gydaytalo@gmail.com>
2020-10-08 21:44:35 -0300

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,8 +4,23 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-})
+}) 
 export class HomePage {
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicar amigos'},
+    { icon: 'phone-portrait-outline', text: 'Recarga de celular'},
+    { icon: 'wallet-outline', text: 'Depositar'},
+    { icon: 'options-outline', text: 'Ajustar limite'},
+    { icon: 'help-circle-outline', text: 'Me ajuda'},
+    { icon: 'barcode-outline', text: 'Pagar'},
+    { icon: 'lock-open-outline', text: 'Bloquear cartão'},
+    { icon: 'card-outline', text: 'Cartão virtual'},
+  ];
+
+  public slidesOptions = {
+    slidesPerView: 3,
+    freeMode: true
+  };
 
   constructor() {}
 
